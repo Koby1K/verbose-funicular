30d441e9df6de9f0a9036cba140050cd52f20988
Kobyingram11 <kobyingram11@gmail.com>
2022-10-10 22:33:36 -0500

    updated files

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
-require("dotenv").config();
+require("dotenv").config()
 const express = require('express')
 const path = require('node:path')
 const bodyParser = require('body-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-//mongodb+srv://barry:<password>@cluster0.taug6.mongodb.net/?retryWrites=true&w=majority
-
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const { ObjectId } = require('mongodb');
 
-app.use(bodyParser.json())
+dotenv.config({path:'config.env'});
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
+
+MongoClient.connect(process.env.MONGO_URI, { 
+    useUnifiedTopology: true}) 
+    .then(client =>{
+      console.log('Connected to Database')
+      const db = client.db('KingramQuebec')
+      const playersCollection = db.collection('players')  
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', async function (req, res) {
+    res.sendFile(path.join(__dirname, "index.ejs" ));
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
     // res.sendFile(path.join(__dirname, "index.html" )); 
     // res.send('Hello ' + userName + ' from Node/Express/Heroku');
 
-    client.connect;
-
-    const collection = client.db("myFirstDatabase").collection("posts");
-    console.log('connected!');
-        // perform actions on the collection object
-       
-        const result = await collection.findOne(); //.toArray();
-         console.log(result);
-            
-            res.send(result.title);
-            // client.close();
-       
-    
-        // res.send(`Hello Express from inside my client connect f/n!`); 
-});
-
-    
-
-    // res.render('index',  {     }    );
+  
 
 
 app.listen(process.env.PORT || 3000,
   () => console.log(`server is running on port: ${process.env.PORT}` ));
 
-
+MongoClient.connect('mongodb-connection-string', (err, client) => {
+    // ... do something here
+})
