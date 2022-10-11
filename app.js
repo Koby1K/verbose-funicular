1d9374c606c5ba420e0a94b8a969a9075422341c
Kobyingram11 <kobyingram11@gmail.com>
2022-10-10 22:34:21 -0500

    updates

--- app.js
+++ app.js
@@ -16,26 +16,61 @@ MongoClient.connect(process.env.MONGO_URI, {
     .then(client =>{
       console.log('Connected to Database')
       const db = client.db('KingramQuebec')
-      const playersCollection = db.collection('players')  
+      const quotesCollection = db.collection('miscquotes')  
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', async function (req, res) {
-    res.sendFile(path.join(__dirname, "index.ejs" ));
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-    // res.sendFile(path.join(__dirname, "index.html" )); 
-    // res.send('Hello ' + userName + ' from Node/Express/Heroku');
+app.get('/', (req, res) => {
+    db.collection('miscquotes').find().toArray()
+      .then(quotes => {
+        res.render('index.ejs', { quotes: quotes })
+      })
+      .catch(/* ... */)
+  })
 
-  
+  app.post('/miscquotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+      .then(result => {
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+  })
+
+  app.put('/miscquotes', (req, res) => {
+    quotesCollection.findOneAndUpdate(
+      { name: 'BarrySan' },
+      {
+        $set: {
+          name: req.body.name,
+          quote: req.body.quote
+        }
+      },
+      {
+        upsert: true
+      }
+    )
+      .then(result => res.json('Success'))
+      .catch(error => console.error(error))
+  })
+
+  app.delete('/miscquotes', (req, res) => {
+    quotesCollection.deleteOne(
+      { name: req.body.name }
+    )
+      .then(result => {
+        if (result.deletedCount === 0) {
+          return res.json('No quote to delete')
+        }
+        res.json('Deleted Darth Vadar\'s quote')
+      })
+      .catch(error => console.error(error))
+  })
 
 
 app.listen(process.env.PORT || 3000,
   () => console.log(`server is running on port: ${process.env.PORT}` ));
 
-MongoClient.connect('mongodb-connection-string', (err, client) => {
-    // ... do something here
 })
