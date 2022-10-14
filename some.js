aad2a2c78bd620fcb428354588d9c2168e298111
Barry <barrycumbie@gmail.com>
2022-10-14 14:51:45 -0500

    variable mismatch

--- some.js
+++ some.js
@@ -1,13 +1,4 @@
-require('dotenv').config
-const express = require('express')
-const path = require('node:path')
-const bodyParser = require('body-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
-// const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// *CODE BREAKS @ THE ABOVE LINE OF CODE* 
+require("dotenv").config();
 console.log(process.env.MONGO_URI)
 
-const app = express()
 
